96b6c457764d8091a3a9cc8881fb0610647b0e58
Krishanu Das <dkrishanu94@gmail.com>
2026-04-16 12:56:22 +0530

    Adding docker file

--- backend/config/database.py
+++ backend/config/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:1234@localhost/mydatabase"
+DATABASE_URL = "postgresql://postgres:1234@localhost:5432/mydatabase"
 
 engine = create_engine(DATABASE_URL)
 
@@ -10,5 +10,5 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
-
+print("DATABASE_URL =", DATABASE_URL)
 Base = declarative_base()
--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,22 @@
-from fastapi import FastAPI,UploadFile,File
+from fastapi import FastAPI, UploadFile, File
 from typing import List
 from routes import fileRoutes
 from config.database import Base, engine
-from models.file import File
+from models.file import File as FileModel
 from fastapi.middleware.cors import CORSMiddleware
 
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # for development
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(fileRoutes.router)
+Base.metadata.create_all(bind=engine)
+
+print(Base.metadata.tables)
